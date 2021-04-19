36cb5a979d757ca99e1d43d64f5a7e97cb7617d4
schmalz <schmalz@live.co.uk>
2021-04-19 15:23:53 +0100

    Initial commit.

---
+++ adventure.lisp
@@ -0,0 +1,55 @@
+(defparameter *nodes* '((living-room (you are in the living-room. a wizard is snoring loudly on the couch.))
+                        (garden (you are in a beautiful garden. there is a well in front of you.))
+                        (attic (you are in the attic. there is a giant welding torch in the corner.))))
+
+(defparameter *edges* '((living-room (garden west door)
+                                     (attic upstairs ladder))
+                        (garden (living-room east door))
+                        (attic (living-room downstairs ladder))))
+
+(defparameter *objects* '(bucket chain frog whiskey))
+
+(defparameter *object-locations* '((bucket living-room)
+                                   (chain garden)
+                                   (frog garden)
+                                   (whiskey living-room)))
+
+(defparameter *location* 'living-room)
+
+(defun describe-location (loc nodes)
+  (cadr (assoc loc nodes)))
+
+(defun describe-path (edge)
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+
+(defun describe-paths (loc edges)
+  (apply #'append
+         (mapcar #'describe-path
+                 (cdr (assoc loc edges)))))
+
+(defun objects-at (loc objs obj-locs)
+  (flet ((at-loc-p (obj)
+           (eq (cadr (assoc obj obj-locs))
+               loc)))
+    (remove-if-not #'at-loc-p objs)))
+
+(defun describe-objects (loc objs obj-locs)
+  (flet ((desc-obj (obj)
+           `(there is a ,obj on the floor.)))
+    (apply #'append
+           (mapcar #'desc-obj
+                   (objects-at loc objs obj-locs)))))
+
+(defun look ()
+  (append (describe-location *location* *nodes*)
+          (describe-paths *location* *edges*)
+          (describe-objects *location* *objects* *object-locations*)))
+
+(defun walk (direction)
+  (let ((next (find direction
+                    (cdr (assoc *location* *edges*))
+                    :key #'cadr)))
+    (if next
+      (progn (setf *location* (car next))
+             (looK))
+      '(you can not go that way))))
