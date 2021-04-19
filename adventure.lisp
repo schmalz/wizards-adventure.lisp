3533d20a645c5d39551a38e46b1d4f0f59b4a212
schmalz <schmalz@live.co.uk>
2021-04-19 16:39:53 +0100

    Add item pickup.

--- adventure.lisp
+++ adventure.lisp
@@ -50,6 +50,15 @@
                     (cdr (assoc *location* *edges*))
                     :key #'cadr)))
     (if next
-      (progn (setf *location* (car next))
-             (looK))
-      '(you can not go that way))))
+        (progn (setf *location* (car next))
+               (look))
+        '(you can not go that way))))
+
+(defun pickup (object)
+  (cond ((member object
+                 (objects-at *location* *objects* *object-locations*))
+         (push (list object 'body)
+               *object-locations*)
+         `(you are now carrying the ,object))
+        (t
+         '(you can not get that.))))
